92f9fe0fdf6645b57e517b9a7e54a55ade427d5b
Joseph Petersen <josephp90@gmail.com>
2017-08-25 23:24:45 +0200

    wip

--- src/main/java/nl/topicus/bitbucket/api/HttpClientFactory.java
+++ src/main/java/nl/topicus/bitbucket/api/HttpClientFactory.java
@@ -1,16 +1,55 @@
 package nl.topicus.bitbucket.api;
 
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 @Component
 public class HttpClientFactory
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PullRequestListener.class);
 
     public CloseableHttpClient create()
     {
         return HttpClientBuilder.create().useSystemProperties().build();
+    }
+
+    public CloseableHttpClient createNoSSLVerifier() {
+        SSLContext sslContext = null;
+        try {
+            sslContext = new SSLContextBuilder()
+                    .loadTrustMaterial(null, (x509Certificates, authType) -> true)
+                    .build();
+        } catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {
+            LOGGER.warn("Creating a NO SSL Verifier HTTP client failed", e);
+            return create();
+        }
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.INSTANCE)
+                .register("https", new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE))
+                .build();
+        PoolingHttpClientConnectionManager manager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+        return HttpClientBuilder
+                .create()
+                .useSystemProperties()
+                .setSSLContext(sslContext)
+                .setConnectionManager(manager)
+                .build();
     }
 
 }
